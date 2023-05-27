ade5de010d3c6503078e91f556d98b683a932564
Adarsh Verma <adarshverma549@gmail.com>
2023-05-27 15:26:47 +0530

    main and App This is main components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import MiniApp from './tailwind_MiniApp/MiniApp';
+// import ReactSidebar from './ReactSidebar/ReactSidebar';
+import Portfolio from './PortFolio/Portfolio';
+
+function App() {
+  return (
+    <>
+      {/* <MiniApp/> */}
+      {/* <ReactSidebar/> */}
+      <Portfolio/>
+    </>
+  )
+}
+
+export default App
